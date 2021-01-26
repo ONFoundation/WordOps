f771c06be102633634042104234d8d201a06b874
me <me@metal0.timcast.com>
2021-01-26 16:20:50 -0500

    copy configuration Works!

---
+++ roles/WordOps/files/copy_configuration.sh
@@ -0,0 +1,4 @@
+# copy configuration
+cp -rf /usr/local/lib/python3.*/dist-packages/usr/* /usr/
+cp -rn /usr/local/lib/python3.*/dist-packages/etc/* /etc/
+cp -f /usr/local/lib/python3.*/dist-packages/etc/bash_completion.d/wo_auto.rc /etc/bash_completion.d/wo_auto.rc
